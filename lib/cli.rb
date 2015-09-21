5001d0be92cd704af2c56696f0bd9ed08dce0ef7
Brian Murphy-Dye <brian@murphydye.com>
2015-09-21 17:23:00 -0600

    Add command line option parsing.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,8 +1,51 @@
+require 'optparse'
+
 module Trellor
   class Cli
-    def self.parse(board_and_list=nil, card_name=nil, descript=nil)
+    def self.parse(*args)
+    #def self.parse(board_and_list=nil, card_name=nil, descript=nil)
+      options = OpenStruct.new
+      options.force = false
+      options.verbose = false
+
+      opt_parser = OptionParser.new do |opts|
+        opts.banner = "Usage: trellor [boardname[.listname [cardname [description]]]]"
+
+        opts.separator ""
+        opts.separator "Specific options:"
+
+        # Mandatory argument.
+        opts.on("-v", "--[no-]verbose", "Run verbosely") do |lib|
+          options.verbose = true
+        end
+
+        opts.on("-f", "--force", "Force re-caching") do |lib|
+          options.force = true
+        end
+
+        # No argument, shows at tail.  This will print an options summary.
+        # Try it and see!
+        opts.on_tail("-h", "--help", "Show this message") do
+          puts opts
+          exit
+        end
+
+        # Another typical switch to print the version.
+        opts.on_tail("--version", "Show version") do
+          puts ::Trellor::VERSION
+          exit
+        end
+   
+      end
+      opt_parser.parse!(args)
+
+      process(*args)
+    end
+
+    def self.process(board_and_list=nil, card_name=nil, descript=nil)
       trellor = Trellor.new
       if board_and_list.nil?
+        puts "Boards:", '-'*50
         trellor.boards.each{ |board| puts board.name }
       else
         board_name, list_name = board_and_list.split('.')
