b9c73a852378489acbaf445ae4614c9c55be4335
Brian Murphy-Dye <brian@murphydye.com>
2015-09-29 20:29:53 -0600

    Api change. "trellor board.list" is now "trellor board list".

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,51 +1,33 @@
-require 'optparse'
+require 'trollop'
 require 'pathname'
 
 module Trellor
   class Cli
-    def self.parse(*args)
-      options = OpenStruct.new
-      options.force_cache = false
-      options.verbose = false
-
-      opt_parser = OptionParser.new do |opts|
-        opts.banner = "Usage: trellor [boardname[.listname [cardname [description]]]]"
-
-        opts.separator ""
-        opts.separator "Specific options:"
-
-        # Mandatory argument.
-        opts.on("-v", "--[no-]verbose", "Run verbosely") do |lib|
-          options.verbose = true
-        end
-
-        opts.on("-f", "--force", "Force re-caching") do |lib|
-          options.force_cache = true
-          hash = save_all
-          puts hash
-        end
-
-        # No argument, shows at tail.  This will print an options summary.
-        # Try it and see!
-        opts.on_tail("-h", "--help", "Show this message") do
-          puts opts
-          exit
-        end
-
-        # Another typical switch to print the version.
-        opts.on_tail("--version", "Show version") do
-          puts ::Trellor::VERSION
-          exit
-        end
-   
+    def self.parse
+      @opts = Trollop::options do
+        banner "Usage: trellor [boardname [listname [cardname [description]]]]"
+        version "trellor #{VERSION}"
+        opt :cache, 'Cache (or re-cache)', short: '-c'
+        opt :verbose, 'Run verbosely', short: '-v'
       end
-      opt_parser.parse!(args)
 
-      process(*args)
+      if cache?
+        hash = save_all
+        p hash
+      end
+
+      process(*ARGV)
     end
 
+    private
+
     def self.trellor
-      @trellor ||= Trellor.new
+      unless @trellor
+        verbose_log('creating Trellor instance')
+        @trellor = Trellor.new
+        @trellor.be_verbose = true if @opts[:verbose]
+      end
+      @trellor
     end
     
     def self.homepath
@@ -70,8 +52,10 @@ module Trellor
     end
 
     def self.all
+      verbose_log('downloading all boards and lists')
       hash = {}
       trellor.boards.each do |board|
+        verbose_log('downloading board', board.name)
         lists = {}
         board.lists.each{ |l| lists[l.name] = l.id }
         hash[board.name] = { id: board.id, lists: lists }
@@ -79,12 +63,14 @@ module Trellor
       hash
     end
 
-    def self.process(board_and_list=nil, card_name=nil, descript=nil)
-      if board_and_list.nil?
+    def self.process(board_name=nil, list_name=nil, card_name=nil, descript=nil)
+      verbose_log "board_name", board_name
+      verbose_log "list_name", list_name
+      verbose_log "card_name", card_name
+      if board_name.nil?
         puts "Boards:", '-'*50
         trellor.boards.each{ |board| puts board.name }
       else
-        board_name, list_name = board_and_list.split('.')
         if list_name.nil?
           board = trellor.board(board_name)
           puts "Board: #{board.name}", '-'*50
@@ -97,6 +83,14 @@ module Trellor
           trellor.create_card(board_name, list_name, card_name, descript)
         end
       end
+    end
+
+    def self.verbose_log(*args)
+      $stderr.puts("           ****** #{args.inspect}") if @opts[:verbose]
+    end
+
+    def self.cache?
+      @opts[:cache]
     end
   end
 end
--- lib/trellor.rb
+++ lib/trellor.rb
@@ -5,38 +5,55 @@ require 'trello'
 
 module Trellor
   class Trellor
+    attr_accessor :be_verbose
+
     def client(key=ENV['TRELLOR_KEY'], token=ENV['TRELLOR_TOKEN'])
       @client ||= connect(key, token)
     end
 
     def connect(key=ENV['TRELLOR_KEY'], token=ENV['TRELLOR_TOKEN'])
+      verbose_log("connecting with", key, token)
       Trello::Client.new(
         :developer_public_key => key,
         :member_token => token
       )
     end
 
+    def verbose_log(*args)
+      $stderr.puts("           ****** #{args.inspect}") if be_verbose
+    end
+
     def user()
+      verbose_log('username', ENV['TRELLOR_USERNAME'])
       @user ||= client.find(:members, ENV['TRELLOR_USERNAME'])
     end
 
     def boards
+      verbose_log('getting boards') unless @boards
       @boards ||= user.boards.select{ |b| !b.closed? }
+    end
+    def boards=(boards)
+      @boards = boards
     end
 
     def board(name)
+      boards   # to get verbose log ordering correct
+      verbose_log('getting board', name)
       name = name.downcase
       boards.detect{ |board| board.name.downcase.start_with?(name) }
     end
 
     def list(board_name, list_name)
+      this_board = board(board_name)
+      verbose_log('   getting list', board_name, list_name)
       list_name = list_name.downcase
-      board(board_name).lists.detect do |list|
+      this_board.lists.detect do |list|
         list.name.downcase.start_with?(list_name)
       end
     end
 
     def cards(board_name, list_name)
+      verbose_log('       getting cards for', board_name, list_name)
       list(board_name, list_name).cards
     end
 
